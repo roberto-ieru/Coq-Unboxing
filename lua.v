d7529b64fccadd7cf216a7e8cee0aa7b1292122b
Roberto I <roberto@inf.puc-rio.br>
2021-10-18 11:53:53 -0300

    Conversion Lua to LIR is correctly typed

--- lua.v
+++ lua.v
@@ -54,27 +54,58 @@ Inductive LTyping : IREnvironment -> LE -> Prop :=
 .
 
 
-Fixpoint Lua2Lir (e : LE) : IRE :=
+Fixpoint Lua2Lir (Γ : IREnvironment) (e : LE) : IRE :=
   match e with
   | LENil => IREBox TgNil IRENil
   | LENum n => IREBox TgInt (IRENum n)
-  | LEPlus e1 e2 => IREBox TgInt (IREPlus (IREUnbox TgInt (Lua2Lir e1))
-                                          (IREUnbox TgInt (Lua2Lir e2)))
+  | LEPlus e1 e2 => IREBox TgInt (IREPlus (IREUnbox TgInt (Lua2Lir Γ e1))
+                                          (IREUnbox TgInt (Lua2Lir Γ e2)))
   | LECnst => IREBox TgTbl IRECnst
-  | LEGet e1 e2 => IREGet (IREUnbox TgTbl (Lua2Lir e1))
-                          (IREUnbox TgInt (Lua2Lir e2))
-  | LESet e1 e2 e3 => IRESet (IREUnbox TgTbl (Lua2Lir e1))
-                             (IREUnbox TgInt (Lua2Lir e2))
-                             (Lua2Lir e3)
-  | LEVar var => IREVar var
-  | LEFun var body => IREBox TgFun (IRELamb var IRTStar (Lua2Lir body))
-  | LEApp e1 e2 => IREFunApp (IREUnbox TgFun (Lua2Lir e1)) (Lua2Lir e2)
+  | LEGet e1 e2 => IREGet (IREUnbox TgTbl (Lua2Lir Γ e1))
+                          (IREUnbox TgInt (Lua2Lir Γ e2))
+  (* | LESet e1 e2 e3 => IRESet (IREUnbox TgTbl (Lua2Lir Γ e1))
+                             (IREUnbox TgInt (Lua2Lir Γ e2))
+                             (Lua2Lir Γ e3) *)
+  | LESet e1 e2 e3 => IREBox TgNil (IRESet (IREUnbox TgTbl (Lua2Lir Γ e1))
+                             (IREUnbox TgInt (Lua2Lir Γ e2))
+                             (Lua2Lir Γ e3))
+  | LEVar var =>
+      match In Γ var with
+      | Some _ => IREVar var
+      | None => IREBox TgNil IRENil  (* global variables are always nil *)
+      end
+  | LEFun var body =>
+      IREBox TgFun (IREFun
+         (IRELamb var IRTStar (Lua2Lir (var |=> IRTStar; Γ) body)))
+  | LEApp e1 e2 => IREFunApp (IREUnbox TgFun (Lua2Lir Γ e1)) (Lua2Lir Γ e2)
   end.
 
 
 Definition LuaEnv (Γ : IREnvironment) := forall var T,
-    In Γ var = Some T -> T = IRTStar.
+    In Γ var = Some T -> In Γ var = Some IRTStar.
 
 
-(* Theorem Lua2LirType : forall Γ e T, *)
+Lemma LuaEnvUpdate : forall Γ var, LuaEnv Γ -> LuaEnv (var |=> IRTStar; Γ).
+Proof.
+  unfold LuaEnv.
+  unfold In.
+  intros Γ var HLE var' T HIn.
+  destruct (string_dec var' var); eauto.
+Qed.
+
+
+(* Issues: 1. globals; 2. type of assignments *)
+Lemma Lua2LirTypeAux : forall Γ e, LuaEnv Γ -> Γ |= Lua2Lir Γ e : IRTStar.
+Proof.
+  intros Γ e.
+  generalize dependent Γ.
+  induction e; intros Γ HLE; simpl;
+  try match goal with  |- context [In Γ ?var] =>
+    destruct (In Γ var) eqn:? end;  (* handle variables *)
+  eauto 8 using IRTyping,LuaEnvUpdate.
+Qed.
+
+
+Corollary Lua2LirType : forall e, MEmpty |= Lua2Lir MEmpty e : IRTStar.
+Proof. intros e. eapply Lua2LirTypeAux. discriminate. Qed.
 

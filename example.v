d2d78e3174ca3d38d3c7ebfed4d2da433c5039f2
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2023-01-31 12:06:46 -0300

    Added a complete example of Pallene.

---
+++ example.v
@@ -0,0 +1,82 @@
+Require Import Coq.Strings.String.
+
+Require Import LIR.maps.
+Require Import LIR.pallene.
+
+(*
+  A nice example of λ-Pallene code:
+
+  let A:tbl[tbl[int]] = {} : tbl[tbl[int]] in
+  let B:tbl[int] = (A as tbl[int]) in
+    A[1] = B; B[2] = 10; A[1][2]
+*)
+
+Definition example :=
+    PELet ("A":string) (PTArr (PTArr PTInt)) (PENew (PTArr PTInt))
+    (PELet ("B":string) (PTArr PTInt)
+                        (PECast (PEVar ("A":string)) (PTArr PTInt))
+     (PELet ("":string) PTNil
+       (PESet (PEVar ("A":string)) (PENum 1) (PEVar ("B":string)))
+    (PELet ("":string) PTNil
+       (PESet (PEVar ("B":string)) (PENum 2) (PENum 10))
+       (PEGet (PEGet (PEVar ("A":string)) (PENum 1)) (PENum 2))))).
+       
+
+(* Example is well typed with type int *)
+Goal typeOf MEmpty example = Some PTInt.
+Proof. trivial. Qed.
+
+
+(*
+** Multistep
+*)
+Reserved Notation "m '/' e -->* m1 '/' e1"
+(at level 40, e at level 39, m1 at level 39, e1 at level 39).
+Reserved Notation "m '/' e -->* 'fail'"
+(at level 40, e at level 39).
+
+Inductive multistep : PMem -> PE -> PMem -> PE -> Prop :=
+| MStRefl : forall m e, m / e -->* m / e
+| MStMStep : forall m e m' e' m'' e'',
+    m / e --> m' / e' ->
+    m' / e' -->* m'' / e'' ->
+    m / e -->* m'' / e''
+
+where "m / e -->* m1 / e1" := (multistep m e m1 e1)
+.
+
+(* example evaluates to 10 *)
+Lemma run : exists m, PEmptyMem / example -->* m / (PENum 10).
+Proof with eauto 10 using pstep, PValue.
+  eexists.
+  unfold example.
+  eapply MStMStep.
+  { eapply PStLet1. eapply PStNew. unfold PfreshT. simpl. eauto. }
+  eapply MStMStep...  (* Let *)
+  eapply MStMStep.
+  { eapply PStLet1. simpl. eapply PStCast; eauto using PValue.
+    discriminate. simpl. trivial. }
+  simpl.
+  eapply MStMStep...  (* Let *)
+  eapply MStMStep.
+  { eapply PStLet1. simpl. eapply PStSet. auto using PValue. }
+  simpl.
+  eapply MStMStep...  (* Let *)
+  eapply MStMStep.
+  { eapply PStLet1. eapply PStSet. auto using PValue. }
+  eapply MStMStep...  (* Let *)
+  eapply MStMStep...  (* Get *)
+  simpl.
+  destruct (lir.Index_dec (PToIndex 1) (PToIndex 2)); try easy.
+  destruct (lir.Index_dec (PToIndex 1) (PToIndex 1)); try easy.
+  eapply MStMStep.
+  { eapply PStGet1. eapply PStCast; auto using PValue; easy. }
+  eapply MStMStep...  (* Get *)
+  simpl.
+  destruct (lir.Index_dec (PToIndex 2) (PToIndex 2)); try easy.
+  eapply MStMStep.
+  { eapply PStCast; auto using PValue; easy. }
+  eauto using multistep.
+  Unshelve. all: auto using PValue.
+Qed.
+
